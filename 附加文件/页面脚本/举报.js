150b03f049dc0d92985db1c58ac6378ab6e55d44
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-11-07 15:11:38 +0800

    aaaaaa

--- 附加文件/页面脚本/举报.js
+++ 附加文件/页面脚本/举报.js
@@ -1,7 +1,7 @@
 var callback = arguments[arguments.length - 1];
 const pathParts = window.location.pathname.split('/');
 const userId = pathParts[2]; // 提取路径中的第二部分作为 userId
-const delay = 50
+const delay = 500
 const timeout = 3000
 let pcursor = '';
 
